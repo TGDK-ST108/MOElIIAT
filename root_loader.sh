094e39a33eb652b7abe25cbc09ff6b7abe3dc44f
Sean Tichenor <jtart2133014@gmail.com>
2025-03-27 19:36:44 -0400

    Create root_loader.sh

---
+++ root_loader.sh
@@ -0,0 +1,8 @@
+# Step 3: Phi Biomatrix Enforcement
+PHI=$(grep "phi_scalar=" olivia_scope/tgdkdef_iris.vec | cut -d '=' -f2 | tr -d ' ')
+if [[ "$(echo "$PHI < 0.00001446" | bc)" -eq 1 || "$(echo "$PHI > 0.00001469" | bc)" -eq 1 ]]; then
+  echo "[ROOT] Phi biomatrix out of secure range. Abort."
+  echo "REJECTED" > "$ROOT_FLAG"
+  exit 1
+fi
+echo "[ROOT] Phi biomatrix validated: $PHI"
