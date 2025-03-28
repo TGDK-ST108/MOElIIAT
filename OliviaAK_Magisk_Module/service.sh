93fd5449a31de1f8fe1043af4a4d47a9a62c33e4
Sean Tichenor <jtart2133014@gmail.com>
2025-03-27 20:37:49 -0400

    Update service.sh

--- OliviaAK_Magisk_Module/service.sh
+++ OliviaAK_Magisk_Module/service.sh
@@ -1,3 +1,5 @@
 #!/system/bin/sh
 # OliviaAI Retainer enforcement
 /data/data/com.termux/files/home/olivia/OliviaAI_RetainerMap/boot/root_loader.sh
+#!/system/bin/sh
+/system/bin/python3 /data/adb/modules/root-vortex/root-vortex.py apply
